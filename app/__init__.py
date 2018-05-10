baf6bb763ed8dfb77d8e851ec27748cee4331db7
Dylan <dylan.lewis@zoho.com>
2018-05-10 01:21:43 +0100

    Initial commit - added basic config file

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
