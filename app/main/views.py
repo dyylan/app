c97cf3b3ed8575fe739b031e998e03bed9215697
Dylan <dylan.lewis@zoho.com>
2018-05-10 13:53:40 +0100

    added views

---
+++ app/main/views.py
@@ -0,0 +1,6 @@
+from flask import render_template, session, redirect, url_for, current_app
+from . import main
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
