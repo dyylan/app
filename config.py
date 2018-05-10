a53b05c4a361f1481071f4c68900dbe69a04374f
Dylan <dylan.lewis@zoho.com>
2018-05-10 19:50:23 +0100

    Added database and some configs for dev

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    #MAIL_SERVER = os.environ.get('MAIL_SERVER', '')
-    #MAIL_PORT = int(os.environ.get('MAIL_PORT', ''))
-    #MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
-    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'd.and.t.noreply@gmail.com'
+    MAIL_PASSWORD = 'securepassword'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
@@ -16,7 +16,8 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 class TestingConfig(Config):
     TESTING = True
