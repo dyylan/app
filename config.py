7a7fdbe80842daffe5a29fbbdb4349204796b19a
Dylan <dylan.lewis@zoho.com>
2018-05-25 20:30:06 +0100

    Auth blueprint additions

--- config.py
+++ config.py
@@ -18,8 +18,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 class TestingConfig(Config):
     TESTING = True
