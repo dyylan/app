15445a74518d3ecd9f4d7a9b5af3e9a9fc56b6df
Dylan Lewis <dylan@Cadmus.local>
2019-01-06 23:17:17 +0000

    Minor update to emails

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from .. import db
 from ..models import User
-from ..email import send_email
+from ..emails import send_email
 from .forms import (LoginForm, RegistrationForm, ChangePasswordForm,
                     ChangeEmailForm, PasswordResetRequestForm,
                     PasswordResetForm)
