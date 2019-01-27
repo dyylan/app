959d7f424dfb669e47a600dbfe181871f30c41c9
Dylan Lewis <dylan@Cadmus.local>
2019-01-27 12:40:13 +0000

    Added user roles

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ COPY requirements.txt requirements.txt
 
 RUN python -m venv venv 
 RUN venv/bin/pip install -r requirements.txt 
-RUN venv/bin/pip install gunicorn
+RUN venv/bin/pip install gunicorn pymysql
 
 COPY app app
 COPY migrations migrations
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import User, Role, Post
+from app.models import User, Role, Post, Permission
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role, Post=Post)
+    return dict(db=db, User=User, Role=Role, Post=Post, Permission=Permission)
 
 if __name__ == "__main__":
+    Role.insert_roles()
     app.run()
